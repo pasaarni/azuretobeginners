e401404b57643a55502203eddde44a53c9b3604f
Pasi Aarnilähde <83806781+pasaarni@users.noreply.github.com>
2021-05-19 09:59:50 +0300

    Update main.tf

--- Terraform/Azure/Web Apps/main.tf
+++ Terraform/Azure/Web Apps/main.tf
@@ -1,25 +1,24 @@
-# Created by pasaarni 18.5.2021
+# Created by pasaarni 19.5.2021 kesken
 
 provider "azurerm" {
-  version = "~>2.0"
   features {}
 }
 
-resource "azurerm_resource_group" "WebApps" {
+resource "azurerm_resource_group" "webapps" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
 resource "azurerm_virtual_network" "wappsnet" {
-  name                = var.irtual_network_name
-  location            = azurerm_resource_group.WebApps.location
-  resource_group_name = azurerm_resource_group.WebApps.name
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.webapps.location
+  resource_group_name = azurerm_resource_group.webapps.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "integrationsubnet" {
   name                 = var.network_subnet_name
-  resource_group_name  = azurerm_resource_group.WebApps.name
+  resource_group_name  = azurerm_resource_group.webapps.name
   virtual_network_name = azurerm_virtual_network.wappsnet.name
   address_prefixes     = ["10.0.1.0/24"]
   delegation {
@@ -32,27 +31,27 @@ resource "azurerm_subnet" "integrationsubnet" {
 
 resource "azurerm_subnet" "endpointsubnet" {
   name                 = var.endpoint_subnet_name
-  resource_group_name  = azurerm_resource_group.WebApps.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.webapps.name
+  virtual_network_name = azurerm_virtual_network.wappsnet.name
   address_prefixes     = ["10.0.2.0/24"]
   enforce_private_link_endpoint_network_policies = true
 }
 
 resource "azurerm_app_service_plan" "appserviceplan" {
   name                = var.appservice_plan_name
-  location            = azurerm_resource_group.WebApps.location
-  resource_group_name = azurerm_resource_group.WebApps.name
+  location            = azurerm_resource_group.webapps.location
+  resource_group_name = azurerm_resource_group.webapps.name
 
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Shared D1"
+    size = "D1"
   }
 }
 
 resource "azurerm_app_service" "frontwebapp" {
   name                = var.front_webapp_name
-  location            = azurerm_resource_group.WebApps.location
-  resource_group_name = azurerm_resource_group.WebApps.name
+  location            = azurerm_resource_group.webapps.location
+  resource_group_name = azurerm_resource_group.webapps.name
   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
 
   app_settings = {
@@ -68,36 +67,36 @@ resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegration
 
 resource "azurerm_app_service" "backwebapp" {
   name                = var.app_service_backweb_app_name
-  location            = azurerm_resource_group.WebApps.location
-  resource_group_name = azurerm_resource_group.WebApps.name
+  location            = azurerm_resource_group.webapps.location
+  resource_group_name = azurerm_resource_group.webapps.name
   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
 }
 
 resource "azurerm_private_dns_zone" "dnsprivatezone" {
   name                = var.private_dns_zone_name
-  resource_group_name = azurerm_resource_group.WebApps.name
+  resource_group_name = azurerm_resource_group.webapps.name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "dnszonelink" {
   name = "dnszonelink"
-  resource_group_name = azurerm_resource_group.WebApps.name
+  resource_group_name = azurerm_resource_group.webapps.name
   private_dns_zone_name = azurerm_private_dns_zone.dnsprivatezone.name
-  virtual_network_id = azurerm_virtual_network.vnet.id
+  virtual_network_id = azurerm_virtual_network.wappsnet.id
 }
 
-resource "azurerm_private_endpoint" "privateendpoint" {
-  name                = "backwebappprivateendpoint"
-  location            = azurerm_resource_group.WebApps.location
-  resource_group_name = azurerm_resource_group.WebApps.name
+resource "azurerm_private_endpoint" "wappsendpoint" {
+  name                = var.private_dns_zone_vnet_name
+  location            = azurerm_resource_group.webapps.location
+  resource_group_name = azurerm_resource_group.webapps.name
   subnet_id           = azurerm_subnet.endpointsubnet.id
 
   private_dns_zone_group {
-    name = "privatednszonegroup"
+    name = var.private_dns_zone_group_name
     private_dns_zone_ids = [azurerm_private_dns_zone.dnsprivatezone.id]
   }
 
   private_service_connection {
-    name = "privateendpointconnection"
+    name = var.private_service_connection_name
     private_connection_resource_id = azurerm_app_service.backwebapp.id
     subresource_names = ["sites"]
     is_manual_connection = false
